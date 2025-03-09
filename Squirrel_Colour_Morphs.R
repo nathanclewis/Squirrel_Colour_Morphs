897eb9d72e4f4f647e1324ff930aaa5a38288b56
Nathan Lewis <nathan31lewis@gmail.com>
2025-03-09 13:42:31 -0700

    Added URL Test

--- Squirrel_Colour_Morphs.R
+++ Squirrel_Colour_Morphs.R
@@ -22,27 +22,66 @@
 {
   install.packages("BiocManager")
   BiocManager::install("EBImage")
-}
+  }
 
-### Load Packages -----
+### Load Packages and Settings -----
 
 { #run this line to read all packages
   library(tidyverse) #for tidyverse structure of coding using pipes
-  library(ggplot2) #for tidy plot generation
+  library(furrr) #contains future_map_lgl, which speeds up rowwise check of URLs
+  plan(multisession) #enable parallel processing to speed up certain processes
   library(EBImage) #for image processing
-}
+  library(ggplot2) #for tidy plot generation
+  }
 
 ### Read Data -----
 
 { #run this line to read all data files
   
-  ## Ful dataset for 22,219 records from 2020
-  df_sq_1_31535 <- read_csv("Data/sq_RGB_2020_df_1_31535.csv")
-  head(df_sq_1_31535)
+  ## Completed dataset (including RGBs) with 22,219 records from 2020
+  df_sq_2020_completed <- read_csv("Data/sq_RGB_2020_df_1_31535.csv")
+  head(df_sq_2020_completed)
+  
+  ## All 31,535 records from 2020
+  df_sq_2020 <- read_csv("Data/df_2020_complete_data.csv")
+  head(df_sq_2020)
   
   ## All 31413 records from 2021
-  df_sq_2021 <- read_csv("Data/observations_2021_fixedurls.csv")
-  
-  ## All records from 2020
-  df_sq_2020 <- read_csv("Data/df_2020_complete_data.csv")
+  df_sq_2021 <- read_csv("Data/df_2021_complete_data.csv")
+  head(df_sq_2021)
+  }
+
+### Test Image URLs and remove rows with invalid URLs -----
+
+## Create a function that identifies invalid image URLs
+url_check = function(url_in,t=2){
+  con <- url(url_in)
+  check <- suppressWarnings(try(open.connection(con,open="rt",timeout=t),silent=T)[1])
+  suppressWarnings(try(close.connection(con),silent=T))
+  ifelse(is.null(check),TRUE,FALSE)
+  }  
+
+## Choose a subset to process and remove invalid URLs
+df_sq_2021_noerrors <- df_sq_2021 %>%
+  slice(1:10) %>%
+  mutate(valid_url = future_map_lgl(image_url, url_check))
+
+### Coord extraction test -----
+
+## Function that extracts coordinates from a picture
+locate_box = function(image_url){
+  display(readImage(image_url), 
+          temp(),
+          method = "raster", all = TRUE)
+  locator()
 }
+
+## Apply it to a short list
+df_coordinates = df_sq_2021_noerrors %>%
+  slice(1:3) %>% 
+  rowwise() %>%
+  mutate(picture_info = list(locate_box(image_url)))
+
+## View the selected coords
+df_coordinates %>% 
+  pull(picture_info)
